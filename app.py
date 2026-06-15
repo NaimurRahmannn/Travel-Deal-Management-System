ba0da07b924785a8549df446d7563fb44462ad03
NaimurRahmannn <niamurreiman@gmail.com>
2026-06-15 09:39:43 +0600

    feat: Add stat after_request hook in the app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from config import Config
 from routes.deals_route import deals_bp
 from database.model import db
-
+from utils.stats import record_request
 app = Flask(__name__)
 
 app.config.from_object(Config)
@@ -19,6 +19,11 @@ with app.app_context():
 def home():
     return {"msg": "App is running"}
 
+
+@app.after_request
+def count_request(response):
+    record_request(success=response.status_code < 400)
+    return response
 
 @app.errorhandler(400)
 def bad_request(_error):
